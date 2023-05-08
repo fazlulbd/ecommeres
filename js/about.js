7208a6285e769094b4f5d10592c240ee78875361
fazlulbd <fazlulhoque577@gmail.com>
2023-05-08 15:35:53 +0600

    first commit

---
+++ js/about.js
@@ -0,0 +1,8 @@
+// counter up
+// $("#number_1").countMe(40,65);
+// $("#number_2").countMe(40,65);
+// $("#number_3").countMe(40,65);
+// $("#number_4").countMe(40,65);
+// $("#number_5").countMe(40,65);
+
+// counter up
